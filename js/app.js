737bba8a0d7792f2bf229e47e30f096f40681ff5
EdinsonFabianBastosCamacho <54146067+ebastos-14@users.noreply.github.com>
2026-05-30 23:16:41 -0500

    Change database reference from 'overlay' to 'counters'

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-const statsRef = ref(db, "overlay");
+const statsRef = ref(db, "counters");
 
 onValue(statsRef, (snapshot) => {
 
